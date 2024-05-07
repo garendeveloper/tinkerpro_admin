31a8932be0412742fa4e07071084a325a415eb9d
Reygaren Balingit <garenlopez19jan99@gmail.com>
2024-05-07 12:19:11 +0800

    dynamic pdf reports

--- reports/generate_inventory_pdf.php
+++ reports/generate_inventory_pdf.php
@@ -23,6 +23,7 @@ function autoAdjustFontSize($pdf, $text, $maxWidth, $initialFontSize = 10)
 $products = new ProductFacade();
 $inventory = new InventoryFacade();
 $orders = new OrderFacade();
+$loss_and_damage = new Loss_and_damage_facade();
 
 $counter = 1;
 
@@ -49,8 +50,6 @@ $imageHeight = 15;
 $imageX = 10; // Adjust this value to your desired left margin
 $pdf->Image($imageFile, $imageX, $y = 10, $w = $imageWidth, $h = $imageHeight, $type = '', $link = '', $align = '', $resize = false, $dpi = 300, $palign = '', $ismask = false, $imgmask = false, $border = 0, $fitbox = false, $hidden = false, $fitonpage = false);
 $pdf->SetFont('', 'I', 8);
-
-
 
 
 switch($active_id)
@@ -171,6 +170,68 @@ switch($active_id)
             $pdf->Cell($headerWidths[4], $maxCellHeight, $total, 1, 0, 'R');
             $pdf->SetFont('', '', autoAdjustFontSize($pdf, $total, $headerWidths[4]));
             $pdf->Cell($headerWidths[5], $maxCellHeight, $item['isPaid'] == 1 ? "Yes" : "No", 1, 0, 'C');
+            $pdf->Ln(); 
+            $counter++;
+        }
+        
+        break;
+    case 'tbl_all_lostanddamages':
+        $pdf->SetFont('', 'B', 10);
+        $pdf->Cell(0, 10, 'LIST OF LOST AND DAMAGED REFERENCES', 0, 1, 'R', 0);
+        $pdf->Ln(-5);
+        $pdf->SetFont('', 10);
+        $pdf->Cell(0, 10, "{$shop['shop_name']}", 0, 1, 'R', 0);
+
+        $pdf->Ln(-3);
+        $pdf->SetFont('', 'I', 10); // Bold, size 10
+        $pdf->MultiCell(0, 10, "{$shop['shop_address']}", 0, 'R');
+        $pdf->Ln(-9);
+        $pdf->SetFont('', 'I', 8); // Italic, size 8
+        $pdf->MultiCell(0, 10, "Contact: {$shop['contact_number']}", 0, 'L');
+        $pdf->SetFont('', 8);
+        $pdf->Ln(-9);
+        $current_date = date('F j, Y');
+        $pdf->Cell(0, 10, "Date: $current_date", 0, 'L');
+        $pdf->Ln(-2);
+
+        $items = $loss_and_damage->get_all_lostanddamageinfo();
+        $header = array('No','Reference No.','Date', 'Reason', 'Total Qty', 'Total Cost', 'Overall Cost', 'Note');
+        $headerWidths = array(10, 35, 25, 25, 25, 25, 25, 25);
+        $maxCellHeight = 5;
+        
+        $hexColor = '#FF6900';
+        list($r, $g, $b) = sscanf($hexColor, "#%02x%02x%02x");
+        
+        $pdf->SetFillColor($r, $g, $b);
+        
+        $pdf->SetFont('', 'B', 10);
+        for ($i = 0; $i < count($header); $i++) {
+            $pdf->Cell($headerWidths[$i], $maxCellHeight, $header[$i], 1, 0, 'L', true);
+        }
+        $pdf->Ln();
+        
+        foreach ($items as $item) {
+            $totalCost = '₱' . number_format($item['total_cost'], 2);
+            $overallCost = '₱' . number_format($item['over_all_total_cost'], 2);
+            $originalDate = $item['date_transact'];
+            $date_transact = date("M d, Y", strtotime($originalDate));
+
+            $pdf->Cell($headerWidths[0], $maxCellHeight, $counter, 1, 0, 'L');
+            $pdf->SetFont('', '', autoAdjustFontSize($pdf, $counter, $headerWidths[0]));
+            $pdf->Cell($headerWidths[1], $maxCellHeight, $item['reference_no'], 1, 0, 'L');
+            $pdf->SetFont('', '', autoAdjustFontSize($pdf, $item['reference_no'], $headerWidths[1]));
+            $pdf->Cell($headerWidths[2], $maxCellHeight, $date_transact, 1, 0, 'L');
+            $pdf->SetFont('', '', autoAdjustFontSize($pdf, $date_transact, $headerWidths[2]));
+            $pdf->Cell($headerWidths[3], $maxCellHeight, $item['reason'], 1, 0, 'C');
+            $pdf->SetFont('', '', autoAdjustFontSize($pdf, $item['reason'], $headerWidths[3]));
+            $pdf->Cell($headerWidths[4], $maxCellHeight, $item['total_qty'], 1, 0, 'C');
+            $pdf->SetFont('', '', autoAdjustFontSize($pdf, $item['total_qty'], $headerWidths[4]));
+            $pdf->Cell($headerWidths[4], $maxCellHeight, $totalCost, 1, 0, 'R');
+            $pdf->SetFont('', '', autoAdjustFontSize($pdf, $totalCost, $headerWidths[4]));
+            $pdf->Cell($headerWidths[4], $maxCellHeight, $overallCost, 1, 0, 'R');
+            $pdf->SetFont('', '', autoAdjustFontSize($pdf, $overallCost, $headerWidths[4]));
+            $pdf->Cell($headerWidths[4], $maxCellHeight, $item['note'], 1, 0, 'C');
+            $pdf->SetFont('', '', autoAdjustFontSize($pdf, $item['note'], $headerWidths[4]));
             $pdf->Ln(); 
             $counter++;
         }
