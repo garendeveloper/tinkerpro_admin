fb2cdd77e30c6d882c85256ef4db11b0641abfe5
= <=>
2024-07-04 11:39:22 +0800

    reports

--- reports/generate_inventory_excel.php
+++ reports/generate_inventory_excel.php
@@ -30,41 +30,71 @@ switch ($active_id) {
         $sheet->setCellValue('A1', 'No.');
         $sheet->setCellValue('B1', 'Product');
         $sheet->setCellValue('C1', 'Barcode');
-        $sheet->setCellValue('D1', 'Qty in Store');
-        $sheet->setCellValue('E1', 'Amount Before Tax');
-        $sheet->setCellValue('F1', 'Amount After Tax');
-        $sheet->setCellValue('G1', 'Is Paid');
+        $sheet->setCellValue('D1', 'UOM');
+        $sheet->setCellValue('E1', 'Qty in Store');
+        $sheet->setCellValue('F1', 'Amount Before Tax(Php)');
+        $sheet->setCellValue('G1', 'Amount After Tax(Php)');
+
+        $sheet->getStyle('A1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        $sheet->getStyle('B1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_LEFT);
+        $sheet->getStyle('C1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_LEFT);
+        $sheet->getStyle('D1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        $sheet->getStyle('E1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT);
+        $sheet->getStyle('F1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT);
+        $sheet->getStyle('G1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT);
 
         $headerStyle = [
             'font' => ['bold' => true],
-            'fill' => ['fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID, 'startColor' => ['rgb' => 'FF6900']],
+            'fill' => ['fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID, 'startColor' => ['rgb' => 'D3D3D3']],
         ];
         $sheet->getStyle('A1:G1')->applyFromArray($headerStyle);
 
-        $items = $inventory->get_allInventories();
+        $items = $inventory->get_allInventoriesData();
 
         $counter = 1;
 
-        foreach ($items as $item) {
-            if($item['stock'] !== -1)
-            {
-                $isPaid = $item['isPaid'] === 1 ? "YES" : "NO";
-                $sheet->setCellValue('A' . ($counter + 1), $counter);
-                $sheet->setCellValue('B' . ($counter + 1), $item['prod_desc']);
-                $sheet->setCellValue('C' . ($counter + 1), $item['barcode']);
-                $sheet->setCellValue('D' . ($counter + 1), $item['stock']);
-                $sheet->setCellValue('E' . ($counter + 1), number_format($item['amount_beforeTax'], 2));
-                $sheet->setCellValue('F' . ($counter + 1), number_format($item['amount_afterTax'], 2));
-                $sheet->setCellValue('G' . ($counter + 1), $isPaid);
-    
-                $sheet->getStyle('A' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
-                $sheet->getStyle('B' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_LEFT);
-                $sheet->getStyle('C' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_LEFT);
-                $sheet->getStyle('G' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
-    
-                $counter++;
-            }
+        $total_amountBeforeTax = 0;        
+        $total_amountAfterTax = 0;
+        $total_qty = 0;
+        foreach ($items as $item) 
+        {
+            $total_amountBeforeTax += number_format($item['cost'], 2);
+            $total_amountAfterTax += number_format($item['prod_price'], 2);
+            $total_qty += $item['product_stock'];
+
+            $sheet->setCellValue('A' . ($counter + 1), $counter);
+            $sheet->setCellValue('B' . ($counter + 1), $item['prod_desc']);
+            $sheet->setCellValue('C' . ($counter + 1), $item['barcode']);
+            $sheet->setCellValue('D' . ($counter + 1), $item['uom_name']);
+            $sheet->setCellValue('E' . ($counter + 1), $item['product_stock']);
+            $sheet->setCellValue('F' . ($counter + 1), number_format($item['cost'], 2));
+            $sheet->setCellValue('G' . ($counter + 1), number_format($item['prod_price'], 2));
+
+            $sheet->getStyle('A' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+            $sheet->getStyle('B' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_LEFT);
+            $sheet->getStyle('C' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_LEFT);
+            $sheet->getStyle('D' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+            $sheet->getStyle('E' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT);
+            $sheet->getStyle('F' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT);
+            $sheet->getStyle('G' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT);
+
+            $counter++;
+          
         }
+        $lastRowIndex = $counter + 1; 
+        $sheet->mergeCells('A' . $lastRowIndex . ':D' . $lastRowIndex);
+        $sheet->setCellValue('A' . $lastRowIndex, 'Total');
+        $sheet->getStyle('A' . $lastRowIndex)->getFont()->setBold(true);
+        $sheet->getStyle('A' . $lastRowIndex)->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT);
+        $sheet->setCellValue('E' . $lastRowIndex, number_format($total_qty, 2));
+        $sheet->setCellValue('F' . $lastRowIndex, number_format($total_amountBeforeTax, 2));
+        $sheet->setCellValue('G' . $lastRowIndex, number_format($total_amountAfterTax, 2));
+        $headerStyle = [
+            'font' => ['bold' => true],
+            'fill' => ['fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID, 'startColor' => ['rgb' => 'D3D3D3']],
+        ];
+        $sheet->getStyle('A'.$lastRowIndex.':G'.$lastRowIndex)->applyFromArray($headerStyle);
+
 
         foreach (range('A', 'G') as $column) {
             $sheet->getColumnDimension($column)->setAutoSize(true);
@@ -95,7 +125,7 @@ switch ($active_id) {
 
         $headerStyle = [
             'font' => ['bold' => true],
-            'fill' => ['fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID, 'startColor' => ['rgb' => 'FF6900']],
+            'fill' => ['fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID, 'startColor' => ['rgb' => 'D3D3D3']],
         ];
         $sheet->getStyle('A1:F1')->applyFromArray($headerStyle);
         $counter = 1;
@@ -143,44 +173,79 @@ switch ($active_id) {
         $sheet->setCellValue('B1', 'Reference No.');
         $sheet->setCellValue('C1', 'Date of Transaction');
         $sheet->setCellValue('D1', 'Reason');
-        $sheet->setCellValue('E1', 'Total Qty');
-        $sheet->setCellValue('F1', 'Total Cost');
-        $sheet->setCellValue('G1', 'Overall Cost');
-        $sheet->setCellValue('H1', 'Note');
+        $sheet->setCellValue('E1', 'Note');
+        $sheet->setCellValue('F1', 'Total Qty');
+        $sheet->setCellValue('G1', 'Total Cost');
+        $sheet->setCellValue('H1', 'Overall Cost');
+       
+        $sheet->getStyle('A1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        $sheet->getStyle('B1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        $sheet->getStyle('C1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        $sheet->getStyle('D1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        $sheet->getStyle('E1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_LEFT);
+        $sheet->getStyle('F1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT);
+        $sheet->getStyle('G1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT);
+        $sheet->getStyle('H1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT);
 
 
         $headerStyle = [
             'font' => ['bold' => true],
-            'fill' => ['fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID, 'startColor' => ['rgb' => 'FF6900']],
+            'fill' => ['fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID, 'startColor' => ['rgb' => 'D3D3D3']],
         ];
         $sheet->getStyle('A1:H1')->applyFromArray($headerStyle);
         $counter = 1;
 
+        $over_all_qty = 0;
+        $over_all_cost = 0;
+        $over_all_totalCost = 0;
+
         foreach ($items as $item) {
-            $totalCost = '₱ ' . number_format($item['total_cost'], 2);
-            $overallCost = '₱ ' . number_format($item['over_all_total_cost'], 2);
+            $totalCost = number_format($item['total_cost'], 2);
+            $overallCost = number_format($item['over_all_total_cost'], 2);
             $originalDate = $item['date_transact'];
-            $date_transact = date("M d, Y", strtotime($originalDate));
+            $date_transact = date("m-d-Y", strtotime($originalDate));
 
             $sheet->setCellValue('A' . ($counter + 1), $counter);
             $sheet->setCellValue('B' . ($counter + 1), $item['reference_no']);
             $sheet->setCellValue('C' . ($counter + 1), $date_transact);
             $sheet->setCellValue('D' . ($counter + 1), $item['reason']);
-            $sheet->setCellValue('E' . ($counter + 1), $item['total_qty']);
-            $sheet->setCellValue('F' . ($counter + 1), $totalCost);
-            $sheet->setCellValue('G' . ($counter + 1), $overallCost);
-            $sheet->setCellValue('H' . ($counter + 1), $item['note']);
+            $sheet->setCellValue('E' . ($counter + 1), $item['note']);
+            $sheet->setCellValue('F' . ($counter + 1), $item['total_qty']);
+            $sheet->setCellValue('G' . ($counter + 1), $totalCost);
+            $sheet->setCellValue('H' . ($counter + 1), $overallCost);
 
             $sheet->getStyle('A' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
             $sheet->getStyle('B' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
             $sheet->getStyle('C' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
             $sheet->getStyle('D' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
-            $sheet->getStyle('E' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+            $sheet->getStyle('E' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_LEFT);
             $sheet->getStyle('F' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT);
             $sheet->getStyle('G' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT);
-            $sheet->getStyle('H' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_LEFT);
+            $sheet->getStyle('H' . ($counter + 1))->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT);
+            $over_all_qty += $item['total_qty'];
+            $over_all_cost += $item['total_cost'];
+            $over_all_totalCost += $item['over_all_total_cost'];
             $counter++;
         }
+
+
+        $lastRowIndex = $counter + 1; 
+        $sheet->mergeCells('A' . $lastRowIndex . ':E' . $lastRowIndex);
+        $sheet->setCellValue('A' . $lastRowIndex, 'Total');
+        $sheet->getStyle('A' . $lastRowIndex)->getFont()->setBold(true);
+        $sheet->getStyle('A' . $lastRowIndex)->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT);
+        $sheet->setCellValue('F' . $lastRowIndex, number_format($over_all_qty, 2));
+        $sheet->setCellValue('G' . $lastRowIndex, number_format($over_all_cost, 2));
+        $sheet->setCellValue('H' . $lastRowIndex, number_format($over_all_totalCost, 2));
+        $sheet->getStyle('F' . $lastRowIndex)->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT);
+        $sheet->getStyle('G' . $lastRowIndex)->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT);
+        $sheet->getStyle('H' . $lastRowIndex)->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT);
+
+        $headerStyle = [
+            'font' => ['bold' => true],
+            'fill' => ['fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID, 'startColor' => ['rgb' => 'D3D3D3']],
+        ];
+        $sheet->getStyle('A'.$lastRowIndex.':H'.$lastRowIndex)->applyFromArray($headerStyle);
 
         foreach (range('A', 'H') as $column) {
             $sheet->getColumnDimension($column)->setAutoSize(true);
@@ -218,7 +283,7 @@ switch ($active_id) {
 
         $headerStyle = [
             'font' => ['bold' => true],
-            'fill' => ['fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID, 'startColor' => ['rgb' => 'FF6900']],
+            'fill' => ['fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID, 'startColor' => ['rgb' => 'D3D3D3']],
         ];
         $sheet->getStyle('A1:E1')->applyFromArray($headerStyle);
         $counter = 1;
